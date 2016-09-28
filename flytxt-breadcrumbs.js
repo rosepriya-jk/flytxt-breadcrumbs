48bc42c74aae3c2af9b80c1fbe427b148172d6a0
priyanka <priyanka.chaliyan@flytxt.com>
2016-09-28 12:25:08 +0530

    Add dependency
    Change global variable to flytxt variable

--- flytxt-breadcrumbs.js
+++ flytxt-breadcrumbs.js
@@ -1,6 +1,4 @@
-var instance;
-var crumbs;
-
+var Flytxt = window.Flytxt = window.Flytxt || {};
 Polymer({
   is: 'flytxt-breadcrumbs',
   
@@ -16,19 +14,19 @@ Polymer({
   attached: function() {
     var me = this;
     if (me.show) {
-      instance = this;
+      Flytxt.breadcrumbInstance = this;
     } else {
-      crumbs = Polymer.dom(me).querySelectorAll('flytxt-breadcrumb');
+      Flytxt.crumbs = Polymer.dom(me).querySelectorAll('flytxt-breadcrumb');
     }
-    if (instance) {
-      instance.setCrumbs();
+    if (Flytxt.breadcrumbInstance) {
+      Flytxt.breadcrumbInstance.setCrumbs();
     }
   },
   
   setCrumbs: function() {
     var me = this;
     me.$.crumbs.innerHTML = '';
-    _.forEach(crumbs, function(crumb) {
+    Flytxt.crumbs.forEach(function(crumb) {
       Polymer.dom(me.$.crumbs).appendChild(crumb);
     });
   }
