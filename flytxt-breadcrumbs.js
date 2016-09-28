deb856d4ff693206541f1f4e0c7339ec0e01cc3f
priyanka <priyanka.chaliyan@flytxt.com>
2016-09-28 14:22:13 +0530

    Add null check

--- flytxt-breadcrumbs.js
+++ flytxt-breadcrumbs.js
@@ -1,16 +1,16 @@
 var Flytxt = window.Flytxt = window.Flytxt || {};
 Polymer({
   is: 'flytxt-breadcrumbs',
-  
+
   properties: {
     show: {
       type: Boolean,
       value: false
     },
-    
+
     crumbs: Array
   },
-  
+
   attached: function() {
     var me = this;
     if (me.show) {
@@ -22,12 +22,14 @@ Polymer({
       Flytxt.breadcrumbInstance.setCrumbs();
     }
   },
-  
+
   setCrumbs: function() {
     var me = this;
     me.$.crumbs.innerHTML = '';
-    Flytxt.crumbs.forEach(function(crumb) {
-      Polymer.dom(me.$.crumbs).appendChild(crumb);
-    });
+    if (Flytxt.crumbs) {
+      Flytxt.crumbs.forEach(function(crumb) {
+        Polymer.dom(me.$.crumbs).appendChild(crumb);
+      });
+    }
   }
 });
